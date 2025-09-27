c57583dac1eed6fad3ee42bb1936a052bfb64b5d
Mikel Cantero <canteromikel@gmail.com>
2025-09-26 18:20:04 -0600

    NEW: funcionalidad de tags

---
+++ src/utils/slugify.js
@@ -0,0 +1,12 @@
+// src/utils/slugify.js
+export function slugify(text) {
+  return text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-');
+}
