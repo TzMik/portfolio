76f8908d84a37670d5060e0b0010d4f08ed5545f
Mikel Cantero <canteromikel@gmail.com>
2025-12-09 19:47:34 -0600

    new: index style changed

---
+++ src/data/socialLinks.js
@@ -0,0 +1,26 @@
+export const socialLinks = [
+  {
+    href: "https://www.linkedin.com/in/mikel-cantero-loi/",
+    icon: "fab fa-linkedin-in",
+  },
+  {
+    href: "https://github.com/TzMik",
+    icon: "fab fa-github",
+  },
+  {
+    href: "https://x.com/Tzmik",
+    icon: "fab fa-x-twitter",
+  },
+  {
+    href: "https://www.tiktok.com/@mikelcantero",
+    icon: "fab fa-tiktok",
+  },
+  {
+    href: "https://www.instagram.com/mikelcantero.dev/",
+    icon: "fab fa-instagram",
+  },
+  {
+    href: "https://www.youtube.com/@mikelcanterodev",
+    icon: "fab fa-youtube",
+  },
+];
