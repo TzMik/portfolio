7ff269c3b0e19485b8e540be25d2f07863b9eabf
Mikel Cantero <mikel@superogx.com>
2026-05-29 12:20:50 -0600

    Revert "feat: testimonials forms"
    This reverts commit bbffa5f947b78c10df205d805fba5c39f5922e3f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mikelcantero.dev',
-    adapter: node({
-        mode: 'standalone',
-    }),
 	integrations: [sitemap()],
 });
