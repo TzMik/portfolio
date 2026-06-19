69f82e65e4e054dc8bad19eb3da7847a66c762bc
Mikel Cantero <canteromikel@gmail.com>
2026-06-19 07:52:33 -0600

    change blog pagination ui

--- src/lib/pagination.js
+++ src/lib/pagination.js
@@ -3,7 +3,7 @@ import { getSortedAndFilteredPosts, getTags } from "./blog-posts.js";
 import { getSortedProjects } from "./projects.js";
 
 // Exportamos el tamaño de página para que sea consistente en toda la app
-export const POSTS_PER_PAGE = 10;
+export const POSTS_PER_PAGE = 6;
 export const PAGE_SIZE = 12;
 
 /**
